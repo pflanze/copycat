b0529d0a3b46fb5957595cd34f602cda0eb2d95e
Christian Jaeger <ch@christianjaeger.ch>
2014-04-20 10:03:56 +0100

    |thenelse| works

--- copycat.scm
+++ copycat.scm
@@ -23,6 +23,7 @@
 (defmacro (forth-return . es)
   `(cons* ,@(reverse es) $s))
 
+;; -- functions
 
 (forth-def + (a b)
 	   (forth-return (+ a b)))
@@ -42,6 +43,19 @@
 (forth-def swap (a b)
 	   (forth-return b a))
 
+(forth-def zero? (v)
+	   (forth-return (zero? v)))
+
+(forth-def = (a b)
+	   (forth-return (= a b)))
+
+;; eval
+(forth-def eval (v)
+	   (forth-eval $s v))
+
+
+;; -- side-effecting statements
+
 (forth-def print (v)
 	   (print v)
 	   $s)
@@ -58,8 +72,7 @@
 	   (println v)
 	   $s)
 
-(forth-def eval (v)
-	   (forth-eval $s v))
+;; ----------------------------
 
 (def (forth-apply stack #(symbol? word))
      (let-forthword
@@ -77,25 +90,38 @@
      (if (null? prog)
 	 stack
 	 (let-pair ((item prog*) prog)
-		   (cond ((eq? item ':)
-			  ;; takes 2 arguments from program (name,
-			  ;; prog), not stack
-			  (let ((name (car prog*))
-				(subprog (cadr prog*)))
-			    (table-set! forth-words
-					name
-					(forthword name
-						   0
-						   (lambda ($s)
-						     (forth-eval $s subprog))))
-			    (forth-eval stack (cddr prog*))))
-			 ((symbol? item)
-			  (let ((app (thunk (forth-apply stack item))))
-			    (if (null? prog*)
-				(app)
-				(forth-eval (app) prog*))))
-			 (else
-			  (forth-eval (cons item stack) prog*))))))
+		   (cond 
+		    ((symbol? item)
+		     (case item
+		       ((:)
+			;; takes 2 arguments from program (name,
+			;; prog), not stack
+			(let ((name (car prog*))
+			      (subprog (cadr prog*))
+			      (cont (cddr prog*)))
+			  (table-set! forth-words
+				      name
+				      (forthword name
+						 0
+						 (lambda ($s)
+						   (forth-eval $s subprog))))
+			  (forth-eval stack cont)))
+		       ((thenelse)
+			;; takes 2 arguments from program (truebranch,
+			;; falsebranch), and 1 from stack (test value)
+			(let ((cont (cddr prog*)))
+			  (forth-eval (forth-eval (cdr stack)
+						  (if (car stack)
+						      (car prog*)
+						      (cadr prog*)))
+				      cont)))
+		       (else
+			(let ((app (thunk (forth-apply stack item))))
+			  (if (null? prog*)
+			      (app)
+			      (forth-eval (app) prog*))))))
+		    (else
+		     (forth-eval (cons item stack) prog*))))))
 
 (TEST
  > (forth-eval '() '(4 5 5 *))
@@ -112,4 +138,10 @@
  (6 4)
  > (forth-eval '() '(: square (dup *) 4 square))
  (16)
+ > (forth-eval '(5) '(zero?))
+ (#f)
+ > (forth-eval '(5) '(zero? thenelse (1) (0)))
+ (0)
+ > (forth-eval '(0) '(zero? thenelse (1) (0)))
+ (1)
  )
