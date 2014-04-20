3bcce993b44ad239b75702ee58b5f77f7c2d2715
Christian Jaeger <ch@christianjaeger.ch>
2014-04-20 23:13:14 +0100

    give the language a distinct name

--- copycat.scm
+++ copycat.scm
@@ -1,33 +1,41 @@
-;; This implements a Forth-like language. It uses S-expressions for
-;; basic syntax, and deviates from normal Forth by using lists to
-;; quote subprograms, and maybe a few other changes.
+;; Cat: a concatenative language using S-expressions
+
+;; This implements a concatenative ("Forth-like") language. It uses
+;; S-expressions for basic syntax, and deviates from normal Forth by
+;; using lists to quote subprograms, and maybe a few other changes. I
+;; think it is closer to [Joy][] than Forth.
+;; 
+;; [Joy]: https://en.wikipedia.org/wiki/Joy_(programming_language),
+;;        http://www.latrobe.edu.au/humanities/research/research-projects/past-projects/joy-programming-language
+;; [Forth]: https://en.wikipedia.org/wiki/Forth_(programming_language)
+
 
 ;; table to store the values of words
-(def forth-words
+(def cat-words
      (make-table))
 
-;; Note: currently the forth-words table is used for every word
+;; Note: currently the cat-words table is used for every word
 ;; reference at runtime. This could be made more efficient by moving
 ;; the lookups to a parsing step (symbol creation, store as part of
 ;; symbol data structure). ("Linker step")
 
-(def (forth-word-set! #(symbol? name) val)
-     (table-set! forth-words name val))
+(def (cat-word-set! #(symbol? name) val)
+     (table-set! cat-words name val))
 
 
 ;; We need a way to fall back onto the host system to implement
 ;; primitives, thus provide part of a foreign function interface:
 
-(defstruct forthforeigncall
+(defstruct catforeigncall
   #(natural0? numargs)
   #(procedure? op))
 
 ;; setting a word to a Scheme program
-(defmacro (forth-def name args . body)
+(defmacro (cat-def name args . body)
   (assert* symbol? name
 	   (lambda_
-	    `(forth-word-set! ',name
-			      (forthforeigncall
+	    `(cat-word-set! ',name
+			      (catforeigncall
 			       ,(length (source-code args))
 			       (lambda ,(cons '$s (source-code args))
 				 ;; ^ HEH that |source-code| is
@@ -35,42 +43,42 @@
 				 ;; problem, 'Identifier expected'
 				 ,@body))))))
 
-(defmacro (forth-return . es)
+(defmacro (cat-return . es)
   `(cons* ,@(reverse es) $s))
 
-(defmacro (forth-defhost name args)
-  `(forth-def ,name ,args
-	      (forth-return ,(cons name (source-code args)))))
+(defmacro (cat-defhost name args)
+  `(cat-def ,name ,args
+	      (cat-return ,(cons name (source-code args)))))
 
 ;; -- functions
 
-(forth-defhost + (a b))
-(forth-defhost - (a b))
-(forth-defhost * (a b))
-(forth-defhost / (a b))
+(cat-defhost + (a b))
+(cat-defhost - (a b))
+(cat-defhost * (a b))
+(cat-defhost / (a b))
 
-(forth-def dup (a)
-	   (forth-return a a))
+(cat-def dup (a)
+	   (cat-return a a))
 
-(forth-def drop (a)
+(cat-def drop (a)
 	   $s)
 
-(forth-def swap (a b)
-	   (forth-return b a))
+(cat-def swap (a b)
+	   (cat-return b a))
 
-(forth-defhost zero? (v))
-(forth-defhost = (a b))
-(forth-defhost < (a b))
-(forth-defhost <= (a b))
-(forth-defhost > (a b))
-(forth-defhost >= (a b))
-(forth-def != (a b)
-	   (forth-return (not (= a b))))
-(forth-defhost eq? (a b))
-(forth-def !eq? (a b)
-	   (forth-return (not (eq? a b))))
+(cat-defhost zero? (v))
+(cat-defhost = (a b))
+(cat-defhost < (a b))
+(cat-defhost <= (a b))
+(cat-defhost > (a b))
+(cat-defhost >= (a b))
+(cat-def != (a b)
+	   (cat-return (not (= a b))))
+(cat-defhost eq? (a b))
+(cat-def !eq? (a b)
+	   (cat-return (not (eq? a b))))
 
-(forth-def rot (n)
+(cat-def rot (n)
 	   ;; (letv ((args stack*) (split-at $s n))
 	   ;; 	 (append (cons (last args) (butlast args)) stack*))
 	   ;;or, saving on intermediates:
@@ -87,44 +95,44 @@
 
 ;; -- procedures (for side-effects)
 
-(forth-def eval (v)
-	   (forth-eval $s v))
+(cat-def eval (v)
+	   (cat-eval $s v))
 
-(forth-def nop ()
+(cat-def nop ()
 	   $s)
 
-(forth-def set! (prog name)
-	   (forth-word-set! name prog)
+(cat-def set! (prog name)
+	   (cat-word-set! name prog)
 	   $s)
 
-(forth-def ref (name)
-	   (forth-return (table-ref forth-words name)))
+(cat-def ref (name)
+	   (cat-return (table-ref cat-words name)))
 
-(forth-def thenelse (val truebranch falsebranch)
-	   (forth-eval $s (if val truebranch falsebranch)))
+(cat-def thenelse (val truebranch falsebranch)
+	   (cat-eval $s (if val truebranch falsebranch)))
 
-(forth-def print (v)
+(cat-def print (v)
 	   (print v)
 	   $s)
 
-(forth-def write (v)
+(cat-def write (v)
 	   (write v)
 	   $s)
 
-(forth-def newline ()
+(cat-def newline ()
 	   (newline)
 	   $s)
 
-(forth-def println (v)
+(cat-def println (v)
 	   (println v)
 	   $s)
 
 ;; ----------------------------
 
-(def (forth-apply stack #(symbol? word))
-     (let ((w (table-ref forth-words word)))
-       (if (forthforeigncall? w)
-	   (let-forthforeigncall
+(def (cat-apply stack #(symbol? word))
+     (let ((w (table-ref cat-words word)))
+       (if (catforeigncall? w)
+	   (let-catforeigncall
 	    ((numargs op) w)
 	    (case numargs
 	      ((0) (op stack))
@@ -134,9 +142,9 @@
 	       ;; split-at-reverse?
 	       (letv ((args stack) (split-at stack numargs))
 		     (apply op (cons stack (reverse args)))))))
-	   (forth-eval stack w))))
+	   (cat-eval stack w))))
 
-(def (forth-eval stack prog)
+(def (cat-eval stack prog)
      (if (null? prog)
 	 stack
 	 (let-pair ((item prog*) prog)
@@ -152,13 +160,13 @@
 			(let ((name (car prog*))
 			      (subprog (cadr prog*))
 			      (cont (cddr prog*)))
-			  (forth-word-set! name subprog)
-			  (forth-eval stack cont)))
+			  (cat-word-set! name subprog)
+			  (cat-eval stack cont)))
 		       ((THENELSE)
 			;; takes 2 arguments from program (truebranch,
 			;; falsebranch), and 1 from stack (test value)
 			(let ((cont (cddr prog*)))
-			  (forth-eval (forth-eval (cdr stack)
+			  (cat-eval (cat-eval (cdr stack)
 						  (if (car stack)
 						      (car prog*)
 						      (cadr prog*)))
@@ -167,107 +175,107 @@
 			;; takes 1 argument from program, puts it on
 			;; the stack
 			(let ((cont (cdr prog*)))
-			  (forth-eval (cons (car prog*) stack) cont)))
+			  (cat-eval (cons (car prog*) stack) cont)))
 		       (else
-			(let ((app (thunk (forth-apply stack item))))
+			(let ((app (thunk (cat-apply stack item))))
 			  (if (null? prog*)
 			      (app)
-			      (forth-eval (app) prog*))))))
+			      (cat-eval (app) prog*))))))
 		    (else
-		     (forth-eval (cons item stack) prog*))))))
+		     (cat-eval (cons item stack) prog*))))))
 
 (TEST
- > (forth-eval '() '(4 5 5 *))
+ > (cat-eval '() '(4 5 5 *))
  (25 4)
- > (forth-eval '() '(4 5 5 * -))
+ > (cat-eval '() '(4 5 5 * -))
  (-21)
- > (forth-eval '() '(4 5 dup * -))
+ > (cat-eval '() '(4 5 dup * -))
  (-21)
- > (forth-eval '() '(4 5 swap dup * -))
+ > (cat-eval '() '(4 5 swap dup * -))
  (-11)
 
  ;; sublists are representing sub-programs, which are only evaluated
  ;; on demand:
- > (forth-eval '() '(4 (5) eval))
+ > (cat-eval '() '(4 (5) eval))
  (5 4)
- > (forth-eval '() '(4 (5 1 +) eval))
+ > (cat-eval '() '(4 (5 1 +) eval))
  (6 4)
 
  ;; words can be quoted by way of QUOTE:
- > (forth-eval '() '(QUOTE 1))
+ > (cat-eval '() '(QUOTE 1))
  (1)
- > (forth-eval '() '(QUOTE foo))
+ > (cat-eval '() '(QUOTE foo))
  (foo)
 
  ;; "syntax-based" word definition form: |:| takes a name, and a
  ;; program to its right, syntactically
- > (forth-eval '() '(: square (dup *) 4 square))
+ > (cat-eval '() '(: square (dup *) 4 square))
  (16)
  ;; stack-based word definition form (works like a normal word):
  ;; |set!| takes a program and a name from the stack at runtime. (I
- ;; don't know why the original Forth chooses to use such
+ ;; don't know why the original Cat chooses to use such
  ;; "syntax-based" features when it could do with program and symbol
  ;; quoting and then just words like this, other than visual
  ;; preference.)
- > (forth-eval '() '((dup *) QUOTE sqr set! 4 sqr))
+ > (cat-eval '() '((dup *) QUOTE sqr set! 4 sqr))
  (16)
 
  ;; "syntax-based" branching facility: takes a truebranch and a
  ;; falsebranch to its right, syntactically, as well as a boolean
  ;; value from the stack at runtime.
- > (forth-eval '(5) '(zero?))
+ > (cat-eval '(5) '(zero?))
  (#f)
- > (forth-eval '(5) '(zero? THENELSE (1) (0)))
+ > (cat-eval '(5) '(zero? THENELSE (1) (0)))
  (0)
- > (forth-eval '(0) '(zero? THENELSE (1) (0)))
+ > (cat-eval '(0) '(zero? THENELSE (1) (0)))
  (1)
  ;; stack-based branching facility (works like a normal word): takes
  ;; boolean value, truebranch and falsebranch from the stack at
  ;; runtime
- > (forth-eval '(5) '(zero? (1) (0) thenelse))
+ > (cat-eval '(5) '(zero? (1) (0) thenelse))
  (0)
- > (forth-eval '(0) '(zero? (1) (0) thenelse))
+ > (cat-eval '(0) '(zero? (1) (0) thenelse))
  (1)
  ;; rot takes a number denoting the number of elements to rotate, and
  ;; rotates their position on the stack so that the last of those
  ;; becomes the first:
- > (forth-eval '((no) (yes) #t 7) '(3 rot))
+ > (cat-eval '((no) (yes) #t 7) '(3 rot))
  (#t (no) (yes) 7)
  ;; write a word-based branching facility ourselves, using the
  ;; syntax-based one internally:
- > (forth-eval '() '((3 rot THENELSE (drop eval) (swap drop eval))
+ > (cat-eval '() '((3 rot THENELSE (drop eval) (swap drop eval))
 		     QUOTE if set!))
- > (forth-eval '(5) '(zero? (1) (0) if))
+ > (cat-eval '(5) '(zero? (1) (0) if))
  (0)
- > (forth-eval '(0) '(zero? (1) (0) if))
+ > (cat-eval '(0) '(zero? (1) (0) if))
  (1)
  ;; write a word-based branching facility ourselves, using the
  ;; stack-based one internally:
- > (forth-eval '() '((thenelse) QUOTE if* set!))
- > (forth-eval '(5) '(zero? (1) (0) if*))
+ > (cat-eval '() '((thenelse) QUOTE if* set!))
+ > (cat-eval '(5) '(zero? (1) (0) if*))
  (0)
- > (forth-eval '(0) '(zero? (1) (0) if*))
+ > (cat-eval '(0) '(zero? (1) (0) if*))
  (1)
  ;; alias the branching facility by simply storing it to a different
  ;; word:
- > (forth-eval '() '(QUOTE if* ref QUOTE anotherif set!))
- > (forth-eval '(5) '(zero? (1) (0) anotherif))
+ > (cat-eval '() '(QUOTE if* ref QUOTE anotherif set!))
+ > (cat-eval '(5) '(zero? (1) (0) anotherif))
  (0)
- > (forth-eval '(0) '(zero? (1) (0) anotherif))
+ > (cat-eval '(0) '(zero? (1) (0) anotherif))
  (1)
 
  ;; factorial
- > (forth-eval '(0) '(: fact (dup zero? THENELSE (drop 1) (dup 1 - fact *))))
+ > (cat-eval '(0) '(: fact (dup zero? THENELSE (drop 1) (dup 1 - fact *))))
  ;; or:
- > (forth-eval '(0) '(: fact (dup zero? (drop 1) (dup 1 - fact *) thenelse)))
- > (forth-eval '(0) '(fact))
+ > (cat-eval '(0) '(: fact (dup zero? (drop 1) (dup 1 - fact *) thenelse)))
+ > (cat-eval '(0) '(fact))
  (1)
- > (forth-eval '(1) '(fact))
+ > (cat-eval '(1) '(fact))
  (1)
- > (forth-eval '(2) '(fact))
+ > (cat-eval '(2) '(fact))
  (2)
- > (forth-eval '(3) '(fact))
+ > (cat-eval '(3) '(fact))
  (6)
- > (forth-eval '(20) '(fact))
+ > (cat-eval '(20) '(fact))
  (2432902008176640000)
  )
