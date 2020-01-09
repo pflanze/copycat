c66c7ca27c85a89017ee999b3590a6821d9fd010
Christian Jaeger <ch@christianjaeger.ch>
2020-01-09 17:09:28 +0000

    Support return type declarations and docstrings in primitives
    - add cc-type, |cc-parse-type|, and copycat-invalid-type error type
    - update cc-def etc. to only use the type inputs; type result is
      ignored for now!
    - add ccguestproc, a ccproc, make ccforeigncall a ccproc
    - add |cc-parse-body| to remove any docstring from the body

--- copycat-interpreter.scm
+++ copycat-interpreter.scm
@@ -53,7 +53,8 @@
                                        [fixnum-natural0? got]))
   (defclass (copycat-division-by-zero a b))
   (defclass (copycat-type-error [string? predicate] value))
-  (defclass (copycat-host-error exception)))
+  (defclass (copycat-host-error exception))
+  (defclass (copycat-invalid-type [string? reason])))
 
 (def copycat-stack? (ilist-of any?))
 (def copycat-runtime-result?
@@ -61,13 +62,75 @@
                 copycat-runtime-error?))
 
 
-;; --- Primitives -------------------------------------------
+;; --- Procedures -----------------------------------------
+
+;; Type parsing
+
+(definterface cc-type
+  (defclass (cc-type-unknown)
+    (defmethod (inputs _) '())
+    (defmethod (maybe-results _) #f)
+    (defclass (cc-type/unknown-results [ilist? inputs])
+      (defclass (cc-type/results [ilist? results])
+        (defmethod (maybe-results _) results)))))
+
+(def (cc-parse-type l)
+     -> (Result-of cc-type?
+                   copycat-runtime-error?)
+     (let (l* (cj-desourcify l))
+       (if (list? l*)
+           (if (null? l*)
+               (Ok (cc-type-unknown))
+               (let (parts (list-split l* '->))
+                 (case (length parts)
+                   ((1) (Ok (cc-type/unknown-results
+                             (first parts))))
+                   ((2) (Ok (cc-type/results
+                             (first parts)
+                             (second parts))))
+                   (else
+                    ;; hmm curried functions?
+                    (Error (copycat-invalid-type
+                            l "at most one '-> is allowed"))))))
+           (Error (copycat-invalid-type l "not a list")))))
+
+(TEST
+ > (cc-parse-type '())
+ [(Ok) [(cc-type-unknown)]]
+ > (cc-parse-type '(a b -> b))
+ [(Ok) [(cc-type/results) (a b) (b)]]
+ > (cc-parse-type '(-> any?))
+ [(Ok) [(cc-type/results) () (any?)]]
+ > (cc-parse-type '([list? l] -> any?))
+ [(Ok) [(cc-type/results) ([list? l]) (any?)]]
+ > (cc-parse-type 'foo)
+ [(Error) [(copycat-invalid-type) foo "not a list"]]
+ > (cc-parse-type '([list? l] -> any? -> foo?))
+ [(Error)
+  [(copycat-invalid-type)
+   ([list? l] -> any? -> foo?)
+   "at most one '-> is allowed"]])
+
+
+;; Procedure values
+
+(defclass ((ccproc #f)
+           [(maybe string?) docstring]
+           [cc-type? type]))
+
+;; Defined in guest language:
+
+(defclass (ccguestproc [ilist? code])
+  extends: ccproc)
+
+;; Primitives:
 
 ;; Wich is (also) part of a foreign function interface (?), hence the
 ;; naming:
 
 (defclass (ccforeigncall [natural0? numargs]
-                         [procedure? op]))
+                         [procedure? op])
+  extends: ccproc)
 
 
 ;; like Result:try but converts non-|copycat-runtime-error|s
@@ -117,34 +180,73 @@
   (typed-lambda-expand stx args body '##begin
                        `copycat:type-check-error))
 
+
+(def (cc-parse-body perhaps-docstring+body)
+     -> (values-of (maybe (possibly-source-of string?))
+                   ilist?)
+     (let (perhaps-docstring+body (source-code perhaps-docstring+body))
+       (if-let-pair ((a r) perhaps-docstring+body)
+                    (if (string? (source-code a))
+                        (values a r)
+                        (values #f perhaps-docstring+body))
+                    (values #f perhaps-docstring+body))))
+
 ;; setting a word to a Scheme program (not translating Scheme
-;; exceptions)
-(defmacro (cc-def name args . body)
-  (assert* symbol? name
-           (lambda_
-            `(cc-word-set! ',name
-                           (ccforeigncall
-                            ,(length (source-code args))
-                            (copycat-lambda
-                             ,(cons* '$word '$s (source-code args))
-                             ;; ^ HEH that |source-code| is
-                             ;; required. otherwise gambit has a
-                             ;; problem, 'Identifier expected'
-                             (in-monad Result
-                                       ,@body)))))))
+;; exceptions). body may start with a docstring.
+(defmacro (cc-def name args/type . perhaps-docstring+body)
+  (assert* symbol? name)
+  (if-Ok (cc-parse-type args/type)
+         (let (inputs (.inputs it))
+           (letv ((maybe-docstring body)
+                  (cc-parse-body perhaps-docstring+body))
+                 `(cc-word-set! ',name
+                                (ccforeigncall
+                                 ,maybe-docstring
+                                 ,(.show it) ;; type
+                                 ,(length inputs) ;; numargs
+                                 (copycat-lambda
+                                  ,(cons* '$word '$s inputs)
+                                  ;; ^ HEH that |source-code| is
+                                  ;; required. otherwise gambit has a
+                                  ;; problem, 'Identifier expected'
+
+                                  ;; XX `(-> (Result-of ,(.maybe-results
+                                  ;; it)) ..), no, it's not the result of
+                                  ;; the copycat-lambda, those are inside
+                                  ;; cc-return. TODO: verify anyway, don't
+                                  ;; want unchecked "docs".
+                             
+                                  (in-monad Result
+                                            ,@body))))))
+         (source-error args/type "args/type parsing error"
+                       ;; XX .show ?
+                       it)))
 
 (defmacro (cc-return . es)
   `(Ok (cons* ,@(reverse es) $s)))
 
-(defmacro (cc-defhost name args)
-  `(cc-def ,name ,args
-           (cc-return ,(cons name
-                             (map perhaps-typed.var (source-code args))))))
+(defmacro (cc-defhost name args/type)
+  ;; argh, have to cc-parse-type twice? COPY-PASTE.
+  (if-Ok (cc-parse-type args/type)
+         `(cc-def ,name ,args/type
+                  (cc-return ,(cons name
+                                    (map perhaps-typed.var
+                                         (.inputs it)))))
+         (source-error args/type "args/type parsing error"
+                       ;; XX .show ?
+                       it)))
 
-(defmacro (cc-defhost/try name args)
-  `(cc-def ,name ,args
-           (>>= (copycat:try-Ok ,(cons name (source-code args)))
-                (C cc-return _))))
+(defmacro (cc-defhost/try name args/type)
+  ;; ditto
+  (if-Ok (cc-parse-type args/type)
+         `(cc-def ,name ,args/type
+                  (>>= (copycat:try-Ok ,(cons name
+                                              (map perhaps-typed.var
+                                                   (.inputs it))))
+                       (C cc-return _)))
+         (source-error args/type "args/type parsing error"
+                       ;; XX .show ?
+                       it)))
 
 
 ;; --- Interpreter -------------------------------------------
@@ -153,8 +255,8 @@
      -> copycat-runtime-result?
      (if-Just ((w (table.Maybe-ref cc-words word)))
               (if (ccforeigncall? w)
-                  (let-ccforeigncall
-                   ((numargs op) w)
+                  (let.-static
+                   (ccforeigncall. (numargs op) w)
                    (let (err
                          (lambda ()
                            (Error (copycat-missing-arguments word/loc
