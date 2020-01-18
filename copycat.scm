076db757fad43396a29e23e73b36d6a535cf027f
Christian Jaeger <ch@christianjaeger.ch>
2020-01-18 02:52:31 +0000

    copycat: fix: show turtle updates even when a program fails
    Now the big Q is whether they should be reverted as well. I/O
    versus...

--- copycat.scm
+++ copycat.scm
@@ -24,10 +24,16 @@
       Result
       (pretty-print (cj-desourcify (.stack cci))) ;; XX display modes?
       (display ($ (.fuel cci) " \\$ "))
-      (let (err (lambda (e) ;; takes old cci -- we don't have a new one
-                       ;; anyway, currently
-                  (warn "Error:" (try-show e))
-                  (_cc-repl cci past future (turtle-commands))))
+      (let* ((next (lambda (cci past future)
+                     ;; Hack: proper hook?
+                     (let (tcmds (turtle-commands))
+                       (unless (eq? tcmds last-commands)
+                         (cc-unwrap (cc-interpreter.eval cci '(zeig))))
+                       (_cc-repl cci past future tcmds))))
+             (err (lambda (e) ;; takes old cci -- we don't have a new one
+                    ;; anyway, currently
+                    (warn "Error:" (try-show e))
+                    (next cci past future))))
         (if-Ok (>>= (let (($word 'cc-repl))
                       (copycat:try-Ok
                        (with-input-from-string (read-line)
@@ -44,17 +50,11 @@
                              (`(redo)
                               XXX)
                              (else
-                              (>>= (cc-interpreter.eval cci prog)
-                                   ;; Hack: proper hook?
-                                   (lambda (cci)
-                                     (if (eq? (turtle-commands) last-commands)
-                                         (Ok cci)
-                                         (cc-interpreter.eval cci '(zeig)))))))))
-               (_cc-repl it
-                         (cons it (rappend future (cons cci past)))
-                         ;; ^ XX undo/redo retain fuel, too, now.
-                         '()
-                         (turtle-commands))
+                              (cc-interpreter.eval cci prog)))))
+               (next it
+                     (cons it (rappend future (cons cci past)))
+                     ;; ^ XX undo/redo retain fuel, too, now.
+                     '())
                (err it)))))
 
 (def (cc-repl #!optional (cci (fresh-cc-interpreter))) -> !
