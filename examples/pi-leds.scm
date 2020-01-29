19f0a4e6ffb9549254794aa8a203ec1a8119b961
Christian Jaeger <ch@christianjaeger.ch>
2020-01-29 03:42:01 +0100

    examples/pi-leds: new sudoserver approach

--- examples/pi-leds.scm
+++ examples/pi-leds.scm
@@ -2,25 +2,53 @@
 ;; supports setting LED 0)
 
 
+(: putfile-bashcode [string? contents] [path-string? path] -> string?
+   "Generate bash code to write `contents` to `path`."
+   (
+    shell-quote
+    swap
+    shell-quote "echo " swap " > " 3 list strings-append
+    swap string.append))
 
-(: led-sys-basedir [fixnum-natural0? ledNo] -> path-string?
-   (.string "/sys/devices/platform/leds/leds/led" swap .append))
 
 (: sudo-putfile [string? contents] [path-string? path] ->
    "Like putfile but uses sudo / bash / echo to write the file. NOTE:
 this leads to entries in /var/log/auth.log for every single
 invocation (disk wear/space use)!"
-   (
-    shell-quote
-    swap
-    shell-quote "echo " swap " > " 3 list strings-append
-    swap string.append
+   (putfile-bashcode
     shell-quote "sudo bash -c " swap string.append
     xrun-bash))
 
+
+;; better solution:
+
+(: new-sudoserver (-> port?)
+   "Open a new instance of bash running under sudo, with its stdin
+connected to the returned port."
+   ("sudo" ("bash") open-receiver-command))
+
+(: sudoserver.run ([port? server] [string? code] ->)
+   (over swap port.println port.flush))
+
+(: sudoserver-putfile
+   [port? server] [string? contents] [path-string? path] ->
+   "Like putfile but uses the given sudoserver to write the file."
+   (putfile-bashcode sudoserver.run))
+
+
+new-sudoserver 1 list 'current-sudoserver set!
+
+(: current-putfile
+   (current-sudoserver -rot sudoserver-putfile))
+
+
+
+(: led-sys-basedir [fixnum-natural0? ledNo] -> path-string?
+   (.string "/sys/devices/platform/leds/leds/led" swap .append))
+
 (: led-trigger-none [fixnum-natural0? ledNo] ->
    "Run this first to remove automatic triggers that might be set up."
-   (led-sys-basedir "/trigger" .append "none" swap sudo-putfile))
+   (led-sys-basedir "/trigger" .append "none" swap current-putfile))
 
 (: led-set [fixnum-natural0? ledNo] [uint8? brightness] ->
    "Set brightness of the given LED. (On the current Raspberry Pis,
@@ -28,7 +56,7 @@ anything other than 0 is full brightness.)"
    (
     .string
     swap led-sys-basedir "/brightness" .append
-    sudo-putfile))
+    current-putfile))
 
 
 (: led-blink [fixnum-natural0? ledNo] -> !
